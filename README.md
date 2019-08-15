09bbd0e1a9097b315d1e5308808bb17b83587398
Nicolae Casîr <nicolae.casir@gmail.com>
2019-08-15 04:19:59 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Image Optimizer
+I had to process (optimize, resize, crete thumbnails and delete) more than 150k images.
+The easiest way was to create this python script.
+
+## Usage
+1. Install [PIL](https://pillow.readthedocs.io/en/stable/installation.html)
+    ```
+    pip install Pillow
+    ```
+    or
+    ```
+    python3 -m pip install Pillow
+    ```
+2. Edit file `main.py`:
+    ```python
+    location = "images/"  # Folder to parse
+    valid_ext = [".jpg", "jpeg", ".gif", ".png"]  #  Allowed extensions
+    total = processed = removed = 0  # Counters
+    img_size = (1600, 1200)  # Width, height for image crop
+    thumbnail = (350, 260)  # Width, height for thumbnail
+    quality = 60  # Quality of saved images
+    min_width = 500  # Min with for image to process
+    log_nr = 5  # Every X files, Log total processed
+    ```
+3. Run script:
+```
+python main.py
+```
+or
+``` 
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,75 @@
+import os
+from PIL import Image, ImageOps
+import datetime
+
+location = "images/"  # Folder to parse
+valid_ext = [".jpg", "jpeg", ".png"]  #  Allowed extensions
+total = processed = removed = 0  # Counters
+img_size = (1600, 1200)  # Width, height for image crop
+thumbnail = (350, 260)  # Width, height for thumbnail
+quality = 60  # Quality of saved images
+min_width = 500  # Min with for image to process
+log_nr = 5  # Every X files, Log total processed
+start_time = datetime.datetime.now()  # Start datetime
+
+def parse_folders(folder):
+    global total, processed, removed, min_width, log_nr, quality
+
+    for file in os.listdir(folder):
+        path = os.path.join(folder, file)
+        filename, file_extension = os.path.splitext(path)
+        if file_extension in valid_ext:
+            image = Image.open(path)
+            width, height = image.size
+
+            if width < min_width:
+                # If image width is less than min_width, remove!
+                os.remove(path)
+                removed += 1
+            else:
+                # Check if is landscape
+                if width > height:
+                    # Check if image width is bigger than our width
+                    if width >= img_size[0]:
+                        # Crop Image
+                        original = ImageOps.fit(image, img_size, method=Image.ANTIALIAS, bleed=0.0, centering=(0.5, 0.5))
+                        # Optimize image, set quality to 60% and save
+                        original.save(filename + file_extension, optimize=True, quality=quality)
+                    else:
+                        # Just optimize, set quality to 60% and save
+                        image.save(filename + file_extension, optimize=True, quality=quality)
+                # If image is in portrait mode
+                elif height > width:
+                    if height >= img_size[1]:
+                        original = ImageOps.fit(image, tuple(reversed(img_size)), method=Image.ANTIALIAS, bleed=0.0, centering=(0.5, 0.5))
+                        original.save(filename + file_extension, optimize=True, quality=quality)
+                    else:
+                        image.save(filename + file_extension, optimize=True, quality=quality)
+                # If image is square
+                else:
+                    image.save(filename + file_extension, optimize=True, quality=quality)
+                # Count processed images
+                processed += 1
+
+                # Create thumbnail
+                thumb = ImageOps.fit(image, thumbnail, method=Image.ANTIALIAS, bleed=0.0, centering=(0.5, 0.5))
+                thumb.save(filename + "-thumbnail" + file_extension, optimize=True, quality=quality)
+
+            # Count total images
+            total += 1
+            if total % log_nr == 0:
+                print(str(datetime.datetime.now()) + ": ===== Processed " + str(total) + " images ...")
+        if os.path.isdir(path):
+            # If is folder instead of file, parse this folder
+            parse_folders(path)
+
+
+print(str(datetime.datetime.now()) + ": Parsing folder \"" + location + "\"...")
+parse_folders(location)
+print(str(datetime.datetime.now()) + ": Done...")
+print("Total parsed: " + str(total) + " images.")
+print("Processed: " + str(processed) + " images.")
+print("Removed: " + str(removed) + " images")
+elapsed_seconds = (datetime.datetime.now() - start_time).total_seconds()
+print("Elapsed time: " + str("%.3f" % elapsed_seconds)+ " seconds. Aprox. " + str(int(elapsed_seconds / 60)) + " minutes.")
+print("Bye bye...")
